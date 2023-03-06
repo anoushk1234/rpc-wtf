c1de46ca707ef58802442bad8e69a547e586acd5
Anoushk Kharangate <32778608+anoushk1234@users.noreply.github.com>
2023-03-06 13:08:14 +0530

    Create add.sh

---
+++ add.sh
@@ -0,0 +1,24 @@
+# change entrypoint to testnet or mainnet or whatever if needed
+#  --no-snapshot-fetch \
+# --no-genesis-fetch \
+# these two above commands are required if you already have a snapshot otherwise REMOVE THEM else it wont work
+
+/home/<USER>/<NAME-OF-YOUR-RPC-BINARY>/target/release/solana-validator \
+  --identity ./test-keypair.json \
+  --vote-account ./test-keypair.json \
+  --rpc-port 8899 \
+  --entrypoint entrypoint.devnet.solana.com:8001 \
+  --limit-ledger-size \
+  --no-voting \
+  --no-os-network-limits-test \
+  --minimal-snapshot-download-speed 100000000 \
+  --skip-poh-verify \
+  --no-snapshot-fetch \
+  --no-genesis-fetch \
+  --no-poh-speed-test \
+  --full-rpc-api \
+  --enable-rpc-transaction-history \
+  --log solana-validator.log \
+  --accounts /mnt/accounts \
+  --ledger /mnt/ledger \
+  --snapshots /mnt/snapshots
